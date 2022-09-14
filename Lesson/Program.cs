599d666abdc9f0d586800a266447e14668020b9b
Kieran Wales <17WalesK@fulford.york.sch.uk>
2022-09-14 12:02:49 +0100

    added switch cases

--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -10,7 +10,29 @@ namespace Lesson
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your favourite pizza? ");
+            string flavour = Console.ReadLine().ToLower();
 
+            switch (flavour)
+            {
+                case "cheese and tomato":
+                case "margherita":
+                    Console.WriteLine("Suitable for vegetarians");
+                    break;
+
+                case "pepperoni":
+                case "meat feast":
+                    Console.WriteLine("Not suitable for vegetarians");
+                    break;
+
+                case "":
+                    Console.WriteLine("You didn't even type anything");
+                    break;
+                default:
+                    Console.WriteLine("Unknown pizza. May contain meat!");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
